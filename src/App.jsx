fb5e7578106d512867c335bf8ab5518f0556d17b
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-16 13:41:27 +0530

    Initial setup with React+TailWind+DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-red-700 h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl text-center">Hello world!</h1>
+      <button className="btn mt-5">Click</button>
+    </div>
+  );
+}
+
+export default App;
