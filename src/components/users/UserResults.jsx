a16c0315645dde7de75972996a4e56aade12e706
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-19 19:19:12 +0530

    feat: component to fetch users from URL

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  //! The useEffect hook executes the fetchUsers function once, immediately after the component mounts. It ensures that the user data is fetched and processed when the component is first rendered.
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
+      },
+    });
+
+    // ! This converts the response to json format
+    const data = await response.json();
+
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg: grid-cols-3 md: grid-cols-2">
+        {/* Since the response is stored in a array, we are using the .map function which iterates through each entry in the array */}
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <h3>Loading...</h3>;
+  }
+}
+
+export default UserResults;
