43988c610cf3fe46324c10da84469b891aad9839
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-19 19:20:03 +0530

    feat: added UserResults component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import UserResults from "../components/users/UserResults";
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome Ayush</h1>
-      <p>The URL of github api is {import.meta.env.VITE_GITHUB_URL}</p>
-    </div>
+    <>
+      {/* <h1 className="text-6xl">Welcome Ayush</h1> */}
+      {/* <p>The URL of github api is {import.meta.env.VITE_GITHUB_URL}</p> */}
+
+      {/* //! SEARCH COMPONENT */}
+      <UserResults />
+    </>
   );
 }
 
