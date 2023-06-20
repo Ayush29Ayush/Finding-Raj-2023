4951da7cf567b63a4e675c466e4fae7fb38cedb0
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-20 21:36:26 +0530

    feat: Added UserItem component to display users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -29,7 +31,7 @@ function UserResults() {
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg: grid-cols-3 md: grid-cols-2">
         {/* Since the response is stored in a array, we are using the .map function which iterates through each entry in the array */}
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user}/>
         ))}
       </div>
     );
