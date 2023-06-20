5359434127a20e73d72900a6c05730ba8551fd31
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-20 22:41:30 +0530

    refactor: used createContext hook and code changes

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,37 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
-
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   //! The useEffect hook executes the fetchUsers function once, immediately after the component mounts. It ensures that the user data is fetched and processed when the component is first rendered.
   useEffect(() => {
     fetchUsers();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
-      },
-    });
-
-    // ! This converts the response to json format
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg: grid-cols-3 md: grid-cols-2">
-        {/* Since the response is stored in a array, we are using the .map function which iterates through each entry in the array */}
         {users.map((user) => (
-          <UserItem key={user.id} user={user}/>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
