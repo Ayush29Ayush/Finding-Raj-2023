2e1c60d7a1290fc789d9f78af18f09ad462f2c0e
Ayush Senapati <ayushsenapati123@gmail.com>
2023-07-09 10:15:15 +0530

    feat: Wrapped App with AlertProvider

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,29 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 function App() {
   return (
     //! In React, context is a way to pass data through the component tree without having to manually pass props down through each level. A provider is a component that provides the data to the rest of the components in the tree. To create a context and provider in React, you can use the createContext() and useContext() hooks.
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            {/* //! In react-router-dom v6, switch got replaced with routes and component with element*/}
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              {/* //! In react-router-dom v6, switch got replaced with routes and component with element*/}
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
